16f7049ba6da5166b95576f85204a79b5e8111f3
Chihiro-666 <122085575+Chihiro-666@users.noreply.github.com>
2023-01-09 23:27:50 +0800

    Update code3.cpp

--- code3.cpp
+++ code3.cpp
@@ -1,34 +1,35 @@
 #include <stdio.h>
-int jishu(int a)
-{
-	if((a+1)/2%2==0)
-	{
-		a=a+1;
-		printf("+");
-	}
-	else 
-	{
-		a=a-1;
-		printf("-"); 
-	}
-	return (a);
-}
 int main()
 {
 	int a,step=0;
+	char b[100];
 	scanf("%d",&a);
 	for(step=0;a!=1;step++)
 	{
-		if(a%2==0)
+		if(a%2==0)//偶数用^ 
 		{
 			a=a/2;
-			printf("^");
+			b[step]='^';
 		}
-		else
+		else//奇数的两种情况 
 		{
-			a=jishu(a);
+			if((a+1)/2%2==0&&a!=3)//特殊情况a=3 
+			{
+				a=a+1;
+				b[step]='+';
+			}
+			else 
+			{
+				a=a-1;
+				b[step]='-' ;
+			}
 		}
 	}
-	printf("\n");
-	printf("%d",step);
+	//输出 
+	printf("%d\n",step);
+	for(int i=0;i<step;i++)
+	{
+		printf("%c",b[i]);
+	}
+	return 0;
 }
