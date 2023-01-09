fc8318948ee025de2ffd4417f2c234a3cbcff300
Chihiro-666 <122085575+Chihiro-666@users.noreply.github.com>
2023-01-09 23:26:30 +0800

    Update code5.cpp

--- code5.cpp
+++ code5.cpp
@@ -4,13 +4,15 @@ int main()
 	char a[100];
 	int b[100];
 	int i,count,x,y;
-	long int sum=0;
+	int sum=0;
+	//输入 
 	scanf("%c",&a[0]);
 	for(i=1;a[i-1]!='\n';i++)
 	{
 		scanf("%c",&a[i]);
 	}
 	count=i-1;
+	//移动前的字符串 
 	x=a[count-1];
 	y=a[count-2];
 	for(i=count-3;i>=0;i--)
@@ -19,6 +21,7 @@ int main()
 	}
 	a[1]=x;
 	a[0]=y;
+	//将移动前十六进制数字减5后，将十六进制中的字母转化为数字，如 A=10 
 	for(i=0;i<count;i++)
 	{
 		if(a[i]>=53&&a[i]<=57)
@@ -28,10 +31,12 @@ int main()
 		else
 		b[i]=a[i]-60;
 	}
+	//转化为十进制 
 	for(i=0;i<count;i++)
 	{
 		sum=sum*16+b[i];
 	}
+	//输出 
 	int day,month,year;
 	day=sum%100;
 	month=sum/100%100;
